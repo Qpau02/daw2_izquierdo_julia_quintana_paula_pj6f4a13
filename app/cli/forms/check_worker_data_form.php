aefe023bea4c31ab1cd2d030cd0c2dd4bc509d38
qpau02 <qpau02@gmail.com>
2026-02-10 12:51:09 +0100

    update forms to .php and add the header to it

---
+++ app/cli/forms/check_worker_data_form.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Company Data Management - Check Worker Data</title>
+        <link href="../styles/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <header class="header">
+            <div class="container">
+                <div class="left">
+                    <p><?php echo $_SESSION['userType'] ?></p>
+                </div>
+                <div class="right dropdown">
+                    <button class="dropbtn"><?php echo $_SESSION['user'] ?></button>
+                    <nav class="dropdown-content nav">
+                        <a href="./logout.php">Logout</a>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    </body>
+</html>
